cdc9a94d473dc9542d5ca5d8d85b4440d96f105e
devmcclu <36908944+devmcclu@users.noreply.github.com>
2018-03-02 00:21:24 -0500

    Begin Shooting
    Shooting mechanics are starting to be implimented, with player firing and an enemy node

--- Player.gd
+++ Player.gd
@@ -16,7 +16,17 @@ func _process(delta):
 	var move_left = Input.is_action_pressed("player_move_left")
 	var move_right = Input.is_action_pressed("player_move_right")
 	
+	var fire_gun = Input.is_action_just_pressed("fire_gun")
+	
 	self.look_at(get_global_mouse_position())
+	
+	if fire_gun:
+		print("fire")
+		var newBullet = load("res://bullet.tscn").instance()
+		$"../".add_child(newBullet)
+		newBullet.position = $"bullet_spawn".global_position
+		newBullet.rotation = self.rotation
+		newBullet.linear_velocity = Vector2(cos(self.rotation)*500, sin(self.rotation)*500)
 	
 	if move_up:
 		self.position.y -= 5
