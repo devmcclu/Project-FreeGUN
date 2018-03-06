f5cbbdfdc99a7a50ed4e96c29670d27434a691f8
devmcclu <36908944+devmcclu@users.noreply.github.com>
2018-03-05 23:50:33 -0500

    Simple health system
    Added a simple health system, should be improved upon later as hasty and only works for single player game

---
+++ bullet.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_bullet_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.health -= 1
+		print(body.health)
+		self.queue_free()
---
+++ enemy.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var health = 3
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if self.health == 0:
+		self.queue_free()
+	pass
