ab004434acc5c13026247c7a4bb38442fa371ddb
Devlin McClure <mccluredt@gmail.com>
2018-03-30 11:02:15 -0400

    Begin Proper physics

--- Player.gd
+++ Player.gd
@@ -18,12 +18,6 @@ func _ready():
 	pass
 
 func _process(delta):
-	#Player movement variables
-	var move_up = Input.is_action_pressed("player_move_up")
-	var move_down = Input.is_action_pressed("player_move_down")
-	var move_left = Input.is_action_pressed("player_move_left")
-	var move_right = Input.is_action_pressed("player_move_right")
-	
 	#Player fire variable
 	var fire_gun = Input.is_action_just_pressed("fire_gun")
 	
@@ -33,10 +27,6 @@ func _process(delta):
 	var switch_weapon_1 = Input.is_action_just_pressed("switch_weapon_1")
 	var switch_weapon_2 = Input.is_action_just_pressed("switch_weapon_2")
 	var switch_weapon_3 = Input.is_action_just_pressed("switch_weapon_3")
-	
-	
-	#Player looks at mouse
-	self.look_at(get_global_mouse_position())
 	
 	#Player Fires Weapon
 	if fire_gun:
@@ -55,18 +45,33 @@ func _process(delta):
 	if weapon_down:
 		print("down a weapon")
 	
+#	if self.has_guns[2] == true:
+#		print("new gun pa")
+
+func _physics_process(delta):
+	#Player movement variables
+	var move_up = Input.is_action_pressed("player_move_up")
+	var move_down = Input.is_action_pressed("player_move_down")
+	var move_left = Input.is_action_pressed("player_move_left")
+	var move_right = Input.is_action_pressed("player_move_right")
+	
+	#Player looks at mouse
+	self.look_at(get_global_mouse_position())
+	
 	#Player movement
 	if move_up:
 		self.position.y -= player_speed
+		#self.apply_impulse(Vector2(0,0), Vector2(-player_speed, 0))
 	if move_down:
 		self.position.y += player_speed
+		#self.apply_impulse(Vector2(0,0), Vector2(player_speed, 0))
 	if move_left:
 		self.position.x -= player_speed
+		#self.apply_impulse(Vector2(0,0), Vector2(0, -player_speed))
 	if move_right:
 		self.position.x += player_speed
-	
-#	if self.has_guns[2] == true:
-#		print("new gun pa")
+		#self.apply_impulse(Vector2(0,0), Vector2(0, player_speed))
+	pass
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("gun"):
