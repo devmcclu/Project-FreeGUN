0ee72de779da2cd5c3cd931c3b7f8e3fd0633d6d
Devlin McClure <36908944+devmcclu@users.noreply.github.com>
2018-10-04 23:27:03 -0400

    Create gamestate signals for lobby

--- scripts/gamestate.gd
+++ scripts/gamestate.gd
@@ -16,6 +16,13 @@ var player_name = "The Warrior"
 # Names for remote players in id:name format
 var players = {}
 
+# Signals to let lobby GUI know what's going on
+signal player_list_changed()
+signal connection_failed()
+signal connection_succeeded()
+signal game_ended()
+signal game_error(what)
+
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
