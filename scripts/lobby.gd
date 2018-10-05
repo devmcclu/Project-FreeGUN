01f1ec6eb1a0f5b243361c67dc5277c3638bb855
Devlin McClure <36908944+devmcclu@users.noreply.github.com>
2018-10-04 23:46:30 -0400

    Connect lobby to connection functions

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -7,7 +7,11 @@ extends Control
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	gamestate.connect("connection_failed", self, "_on_connection_failed")
+	gamestate.connect("connection_succeeded", self, "_on_connection_success")
+	gamestate.connect("player_list_changed", self, "refresh_lobby")
+	gamestate.connect("game_ended", self, "_on_game_ended")
+	gamestate.connect("game_error", self, "_on_game_error")
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
