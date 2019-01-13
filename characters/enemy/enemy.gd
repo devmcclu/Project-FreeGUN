eb7ade3b71df9bf7d323f70a53ecd1724dd5f5f1
Devlin McClure <mccluredt@gmail.com>
2019-01-12 22:08:38 -0500

    Clean up player and enemy scripts to remove unneeded code

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -1,39 +1,9 @@
-#extends RigidBody2D
 extends "res://characters/actor.gd"
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-"""
-var health = 100
-var gun_stats = [0, 500, 34]
-var gun_ammo = [1, 0, 0]
-"""
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	if self.health <= 0:
-#		self.queue_free()
-
 func _on_ShootTimer_timeout():
 	self.shoot()
-#	print("fire")
-#	var new_bullet = load("res://bullet/bullet.tscn").instance()
-#	$"../".add_child(new_bullet)
-#	new_bullet.position = $"bullet_spawn".global_position
-#	new_bullet.rotation = self.rotation
-#	new_bullet.linear_velocity = Vector2(cos(self.rotation)*gun_stats[1], sin(self.rotation)*gun_stats[1])
-#	new_bullet.parent = self
-#	if gun_stats[0] > 0:
-#		self.gun_ammo[gun_stats[0]] -= 1
-#		print(gun_ammo[gun_stats[0]])
-#		print("one less")
-#	print(new_bullet.parent)
-
-#func health_check():
-#	if self.health <= 0:
-#		self.queue_free()
--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,25 +1,4 @@
-#extends KinematicBody2D
 extends "res://characters/actor.gd"
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-#signal gun_changed
-#signal ammo_changed
-#signal health_changed
-
-##Base player variables and stats
-#export (int) var speed = 200
-#var velocity = Vector2()
-#var health = 100
-##Array to check if player has a gun
-#var has_guns = [true, false, false]
-##Array to store current gun stats (gun number(gun_stats[0]), bullet speed(gun_stats[1]), damage(gun_stats[2]))
-#var gun_stats = [0, 500, 34]
-##array to store gun ammunition ammount
-#var gun_ammo = [1, 0, 0]
-
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -28,11 +7,6 @@ func _ready():
 	emit_signal("health_changed")
 	$"Camera2D".make_current()
 	$"CanvasLayer/GUI".visible = true
-
-#func health_check():
-#	emit_signal("health_changed")
-#	if self.health <= 0:
-#		self.queue_free()
 
 func _process(delta):
 	#Player fire variable
@@ -95,27 +69,6 @@ func _physics_process(delta):
 	move_and_slide(velocity)
 	#Player looks at mouse
 	self.look_at(get_global_mouse_position())
-
-#func shoot():
-#	if gun_ammo[gun_stats[0]] > 0:
-#		print("fire")
-#		#Bullet scene is loading into game
-#		var new_bullet = load("res://bullet/bullet.tscn").instance()
-#		$"../".add_child(new_bullet)
-#		#Bullet position and rotation is set to the spawn point and rotation on the player
-#		new_bullet.position = $"bullet_spawn".global_position
-#		new_bullet.rotation = self.rotation
-#		#Velocity of the bullet is set to the speed of the weapon's bullets
-#		new_bullet.linear_velocity = Vector2(cos(self.rotation)*gun_stats[1], sin(self.rotation)*gun_stats[1])
-#		#The parent of the bullet is set to the player
-#		new_bullet.parent = self
-#		#Check to see if player still has ammo for all guns besides starting weapon
-#		if gun_stats[0] > 0:
-#			self.gun_ammo[gun_stats[0]] -= 1
-#			print(gun_ammo[gun_stats[0]])
-#			emit_signal("ammo_changed")
-#			print("one less")
-#		print(new_bullet.parent)
 
 #On gun pickup, change gun variables
 func _on_Area2D_area_entered(area):
