1db2b810ffd05fd32333e77fedebb83f57d58bbc
Devlin McClure <36908944+devmcclu@users.noreply.github.com>
2019-03-12 23:25:35 -0400

    Fix where UI is updated for health

--- characters/health.gd
+++ characters/health.gd
@@ -8,7 +8,7 @@ func _ready():
 	emit_signal("health_changed")
 
 func health_check(change):
-	emit_signal("health_changed")
 	health -= change
+	emit_signal("health_changed")
 	if self.health <= 0:
 		self.get_parent().queue_free()
