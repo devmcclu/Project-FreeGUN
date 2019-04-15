ccb081a68e1002dc0e6873615308c4aa2643b451
Devlin McClure <mccluredt@gmail.com>
2019-04-15 18:55:56 -0400

    Fix some missed typings

--- pause_menu/PauseMenu.gd
+++ pause_menu/PauseMenu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-func _input(event) -> void:
+func _input(event : InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = not get_tree().paused
