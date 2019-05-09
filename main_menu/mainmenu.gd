5b4b17c89ea2eb6b8c676cedb02a42f124a9976e
Devlin McClure <mccluredt@gmail.com>
2019-05-09 18:41:42 -0400

    More formatting changes

--- main_menu/mainmenu.gd
+++ main_menu/mainmenu.gd
@@ -1,14 +1,7 @@
 extends Control
 
-func _ready() -> void:
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-
 func _on_Singleplayer_pressed() -> void:
 	get_tree().change_scene("res://levels/Main.tscn")
-
 
 func _on_Multiplayer_pressed() -> void:
 	get_tree().change_scene("res://lobby/lobby.tscn")
--- pause_menu/pausemenu.gd
+++ pause_menu/pausemenu.gd
@@ -2,6 +2,6 @@ extends Control
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		var new_pause_state = not get_tree().paused
+		var new_pause_state: bool = not get_tree().paused
 		get_tree().paused = not get_tree().paused
 		visible = new_pause_state
