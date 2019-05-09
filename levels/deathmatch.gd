cbeb6f51b842dda59c69548c6a7b8fe58b62162f
Devlin McClure <mccluredt@gmail.com>
2019-05-09 19:38:45 -0400

    Start deathmatch implementation

--- levels/deathmatch.gd
+++ levels/deathmatch.gd
@@ -4,12 +4,14 @@ extends Node
 # var a = 2
 # var b = "textvar"
 
+var add_score: bool = true
+
 func _ready() -> void:
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
+#func _process(delta) -> void:
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
