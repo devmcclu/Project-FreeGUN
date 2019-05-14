7bd241ff6fc981b906d7265307940054daffff09
Devlin McClure <mccluredt@gmail.com>
2019-05-14 18:48:54 -0400

    Add ability to store players and score to Deathmatch

--- levels/deathmatch.gd
+++ levels/deathmatch.gd
@@ -1,18 +1,25 @@
 extends Node
 
 var add_score: bool = true
+var score_limit: int = 2
 
 var player_scores: Dictionary = {}
 
 func _ready() -> void:
 	# Add initial player scores
-	for p_id in gamestate.players.keys():
-		self.player_scores[p_id] = 0
+	pass
+#	for p_id in gamestate.players.keys():
+#		self.player_scores[p_id] = 0
+#	print(player_scores.values())
 
 #func _process(delta) -> void:
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
-remote func update_score() -> void:
-	pass
+sync func update_score(player_id: int) -> void:
+	assert(player_id in player_scores)
+	
+func add_player(id, new_player_name):
+	player_scores[id] = { name = new_player_name, score = 0 }
+	print(player_scores.values())
--- scripts/gamestate.gd
+++ scripts/gamestate.gd
@@ -107,6 +107,12 @@ remote func pre_start_game(spawn_points: Dictionary) -> void:
 			player.set_player_name(players[p_id])
 
 		world.get_node("Players").add_child(player)
+		
+	
+	# Set up score
+	world.get_node("DeathmatchMode").add_player(get_tree().get_network_unique_id(), player_name)
+	for pn in players:
+		world.get_node("DeathmatchMode").add_player(pn, players[pn])
 	
 	if not get_tree().is_network_server():
 		# Tell server we are ready to start
