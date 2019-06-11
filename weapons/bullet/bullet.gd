f1984ce31c26ee6151748db4d0ce4bdaab50ff4a
Devlin McClure <mccluredt@gmail.com>
2019-06-11 16:24:25 -0400

    Have bullets get actual parent of themselves
    Bullets were not getting the player character as their parent, so this aims to fix that

--- weapons/bullet/bullet.gd
+++ weapons/bullet/bullet.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-onready var parent = self.get_parent()
+onready var parent = self.get_parent().get_parent()
 
 func _ready() -> void:
 	set_as_toplevel(true)
@@ -15,7 +15,7 @@ func _on_bullet_body_entered(body: PhysicsBody2D) -> void:
 			if self.parent != body:
 #				body.health -= parent.gun_stats[2]
 #				body.health_check()
-				body.get_node("Health").health_check(parent.damage)
+				body.get_node("Health").health_check(parent.get_node("Gun").damage)
 				print(body.get_node("Health").health)
 				self.queue_free()
 		#Wall collision layer
